8a6ccc7dcea280031c4e4de645c1da124e1c741c
Joseph P Kehoe <joe@JosephsAir.local>
2015-04-25 14:47:58 +0100

    Initial Commit

---
+++ SugarScape/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SugarScape
+//
+//  Created by Joseph P Kehoe on 25/04/2015.
+//  Copyright (c) 2015 Joseph P Kehoe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
