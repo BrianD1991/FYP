e4ec364d1f75e002819f366f06c12ea5b7c8f569
Joseph Kehoe <Joseph.Kehoe@itcarlow.ie>
2015-10-19 15:10:44 +0100

    restructured for linux

--- SugarScape/main.cpp
+++ SugarScape/main.cpp
@@ -6,6 +6,24 @@
 //  Copyright (c) 2015 Joseph P Kehoe. All rights reserved.
 //
 
+//
+// Disclamer:
+// ----------
+//
+// This code will work only if you selected window, graphics and audio.
+//
+// Note that the "Run Script" build phase (XCode) will copy the required frameworks
+// or dylibs to your application bundle so you can execute it on any OS X
+// computer.
+//
+// Your resource files (images, sounds, fonts, ...) are also copied to your
+// application bundle. To get the path to these resource, use the helper
+// method resourcePath() from ResourcePath.hpp
+//
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
 #include <chrono>
 #include <iostream>
 #include <fstream>
@@ -28,9 +46,130 @@
 #include "AgentMating.h"
 #include "AgentLoanPayments.h"
 #include "AgentCredit.h"
+#include "ViewPort.h"
+
 
 
 int benchmark(int,int,int,int,int,std::string);
+int Gui(int, float);
+
+
+int Gui(int Dimensions, float pause)
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "SFML window");
+    // create everything
+    World theWorld(Dimensions);
+    theWorld.init();
+    theWorld.sync();
+    theWorld.sanityCeck();
+    ViewPort theGUI(&window,&theWorld,std::pair<int,int>(1024, 768),std::pair<int,int>(0,0),theWorld.getSize());
+    Growback growback(&theWorld);
+   SeasonalGrowback seasonalGrowback(&theWorld);
+    AgentMove move(&theWorld);
+    PollutionFormation pollForm(&theWorld);
+    GarbageCollection gc(&theWorld);
+    AgentCulture agentCulture(&theWorld);
+    AgentDeath agentDeath(&theWorld);
+    AgentDisease agentDisease(&theWorld);
+    Diffusion diffusion(&theWorld);
+    AgentCombat agentCombat(&theWorld);
+    AgentReplacement agentReplacement(&theWorld);
+    AgentMating agentMating(&theWorld);
+    AgentMetabolism agentMetabolism(&theWorld);
+    AgentCredit agentCredit(&theWorld);
+    AgentLoanPayments agentLoanPayments(&theWorld);
+    
+    //!
+    /*!
+     Add the rules we are using here.
+     */
+    theWorld.addRule(&growback);
+    //theWorld.addRule(&seasonalGrowback);
+    //theWorld.addRule(&pollForm);
+    //theWorld.addRule(&diffusion);
+    
+    theWorld.addRule(&move);
+    theWorld.addRule(&agentMating);
+    //theWorld.addRule(&agentCombat);
+    //theWorld.addRule(&agentCulture);
+    //theWorld.addRule(&agentDisease);
+    //theWorld.addRule(&agentReplacement);
+    theWorld.addRule(&agentMetabolism);
+    theWorld.addRule(&agentDeath);
+    //theWorld.addRule(&gc);
+    int stepCount=0;
+    std::string counter;
+    
+    // Set the Icon
+    sf::Image icon;
+    if (!icon.loadFromFile("resources/icon.png")) {
+        return EXIT_FAILURE;
+    }
+    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+    // Create a graphical text to display
+    sf::Font font;
+    if (!font.loadFromFile("resources/sansation.ttf")) {
+        return EXIT_FAILURE;
+    }
+    counter = std::to_string(stepCount);
+    sf::Text text(counter, font, 50);
+    text.setColor(sf::Color::White);
+    
+    // Load a music to play
+    sf::Music music;
+    if (!music.openFromFile("resources/nice_music.ogg")) {
+        return EXIT_FAILURE;
+    }
+    
+    // Play the music -it never hurts!
+    music.play();
+    //!
+    /*!
+     set number of threads here - for testing only. Not needed normally
+     */
+    //omp_set_num_threads(1);
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            
+            // Escape pressed: exit
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+                window.close();
+            }
+        }
+        
+        // Clear screen
+        window.clear();
+        
+        theWorld.applyRules();
+        theGUI.draw();
+        // Draw the sprite
+        //window.draw(sprite);
+        
+        // Draw the string
+        window.draw(text);
+        
+        // Update the window
+        window.display();
+        counter = std::to_string(++stepCount);
+        text.setString(counter);
+        sf::Time t1 = sf::seconds(pause);
+        sf::sleep(t1);
+        theWorld.sanityCeck();
+    }
+    return stepCount;
+}
+
+
 
 int benchmark(int numRepeats, int stepCount, int dimStart, int increment, int runs, std::string fileName){
     std::ofstream outputFile(fileName,std::ios::out | std::ios::app);
@@ -113,10 +252,13 @@ int benchmark(int numRepeats, int stepCount, int dimStart, int increment, int ru
     return 0;
 }
 
+
+
+
 int main(int, char const**)
 {
-    
-    benchmark(1,50, 18, 18, 6, "/Users/joseph/test18-108.txt");
-    return 0;
+    Gui(30,1.0);
+    //benchmark(1,50, 18, 18, 6, "/Users/joseph/test18-108.txt");
+    return EXIT_SUCCESS;   
 }
 
